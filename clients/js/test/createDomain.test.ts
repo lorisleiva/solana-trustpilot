38ab8e1c5aa8f2c85eb58222259e69e3d4df9d1c
Loris Leiva <loris.leiva@gmail.com>
2023-10-01 17:45:07 +0100

    Update existing tests

---
+++ clients/js/test/createDomain.test.ts
@@ -0,0 +1,26 @@
+import { PublicKey } from '@metaplex-foundation/umi';
+import test from 'ava';
+import { Domain, Key, createDomain, fetchDomain, findDomainPda } from '../src';
+import { createUmi } from './_setup';
+
+test('it can create a new domain account', async (t) => {
+  // Given a Umi instance.
+  const umi = await createUmi();
+
+  // When we create a new domain.
+  const [domainPda] = findDomainPda(umi, { domainName: 'example.com' });
+  await createDomain(umi, {
+    domain: domainPda,
+    domainName: 'example.com',
+  }).sendAndConfirm(umi);
+
+  // Then an account was created with the correct data.
+  t.like(await fetchDomain(umi, domainPda), <Domain>{
+    publicKey: domainPda,
+    key: Key.Domain,
+    totalStars: 0n,
+    totalReviews: 0n,
+    reviewers: [] as PublicKey[],
+    domainName: 'example.com',
+  });
+});
